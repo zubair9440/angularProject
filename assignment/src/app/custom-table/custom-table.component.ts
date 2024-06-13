a70412ac5d78fa59c1d34daf10e90beafa05b3a0
zubair.mohammed <zubair.mohammed@pal.tech>
2024-06-13 18:10:00 +0530

    created new angular project

---
+++ assignment/src/app/custom-table/custom-table.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+@Component({
+  selector: 'app-custom-table',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './custom-table.component.html',
+  styleUrl: './custom-table.component.scss'
+})
+export class CustomTableComponent {
+  myObj=[
+    {
+      transactionType: 'termination',
+      duedate: '2021-07-01',
+      daysdue:1
+    },
+    {
+      transactionType: 'termination',
+      duedate: '2021-07-01',
+      daysdue:1
+    },
+    {
+      transactionType: 'termination',
+      duedate: '2021-07-01',
+      daysdue:1
+    },
+    {
+      transactionType: 'termination',
+      duedate: '2021-07-01',
+      daysdue:1
+    },
+    {
+      transactionType: 'termination',
+      duedate: '2021-07-01',
+      daysdue:1
+    }
+  ]
+}
---
+++ assignment/src/app/side-container/side-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-container.component.html',
+  styleUrl: './side-container.component.scss'
+})
+export class SideContainerComponent {
+
+}
